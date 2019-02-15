4006d14e2d4dfe03f6c17e8257d11d6557926e0f
Kapil Kumar <masterkapilkumar@gmail.com>
2019-02-15 16:00:51 +0530

    Prototype added

---
+++ Company.java
@@ -0,0 +1,18 @@
+class Company {
+    
+    public void AddEmployee(Employee E, Employee E1) {
+
+    }
+
+    public Employee DeleteEmployee(Employee E, Employee E1) {
+
+    }
+    
+    public Employee LowestCommonBoss(Employee E, Employee E1) {
+
+    }
+
+    public void printEmployees() {
+
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,10 @@
+class Employee {
+    private String name;
+    private int level;
+
+    public String getName() { return name; }
+    public int getLevel() { return level; }
+
+    public void setName(String name) { this.name = name; }
+    public void setLevel(String level) { this.level = level; }
+}
